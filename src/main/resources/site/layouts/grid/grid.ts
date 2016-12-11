7eb63a74126da56f19f084ddd217cd0227752172
Christian Westgaard <cwe@enonic.com>
2016-12-11 07:55:21 +0100

    Rename example to grid

--- src/main/resources/site/layouts/grid/grid.ts
+++ src/main/resources/site/layouts/grid/grid.ts
@@ -2,14 +2,16 @@
 /// <reference path="../../lib/xp/global.ts" />
 import { LayoutController } from '../../lib/xp/layout/controller';
 
-const layoutName = 'example';
-const viewFile = resolve(`${layoutName}.html`);
+const name = 'grid';
+const type = 'layout';
+const viewFile = resolve(`${name}.html`);
 
 export default class ExampleLayoutController extends LayoutController {
 
     constructor(request: any) {
         super(request);
-        this.componentName = layoutName;
+        this.name = name;
+        this.type = type;
         this.viewFile = viewFile;
     }
 
