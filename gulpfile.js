6a843b17e5936e6ca8c5ae54e454f5f8c851cf11
Christian Westgaard <cwe@enonic.com>
2016-12-11 15:23:00 +0100

    Build sass -> css too.

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,12 @@ var ignoreGlob = srcResourcesDir + '/**/*.{js,scss,ts}';
 var copyFiles = glob.sync(srcGlob, { absolute: true, ignore: ignoreGlob });
 var copyTasks = copyFiles.map(f => 'copy:'+ f);
 
+var scssGlob = srcResourcesDir + '/**/*.scss';
+var mainScssGlob = srcResourcesDir + '/assets/scss/styles.scss';
+var mainScssFile = glob.sync(mainScssGlob, { absolute: true })[0];
+var scssFiles = glob.sync(scssGlob, { absolute: true });
+console.log('scssFiles:' + JSON.stringify(scssFiles, null, 4));
+
 var tsGlob = srcResourcesDir + '/**/*.ts';
 var dtsGlob = srcResourcesDir + '/**/*.d.ts';
 var tsFiles = glob.sync(tsGlob, { absolute: true, ignore: dtsGlob });
@@ -33,6 +39,10 @@ var tsBuildTasks = tsFiles.map(f => 'build:'+ f);
 var watchFiles = copyFiles.concat(tsFiles);
 //console.log('dtsFiles:' + JSON.stringify(dtsFiles, null, 4));
 //process.exit()
+
+//──────────────────────────────────────────────────────────────────────────────
+// xml, html, svg, etc...
+//──────────────────────────────────────────────────────────────────────────────
 
 copyFiles.forEach(function (filePath) {
     function copyFile() {
@@ -48,6 +58,10 @@ copyFiles.forEach(function (filePath) {
         done();
     });
 });
+
+//──────────────────────────────────────────────────────────────────────────────
+// ts -> js
+//──────────────────────────────────────────────────────────────────────────────
 
 tsFiles.forEach(function(tsFile) {
 
@@ -72,6 +86,10 @@ tsFiles.forEach(function(tsFile) {
 
 }); // tsFiles.forEach
 
+//──────────────────────────────────────────────────────────────────────────────
+// Copy package.json dependencies
+//──────────────────────────────────────────────────────────────────────────────
+
 gulp.task('build:node_modules', function () {
     return gulp.src(
         Object.keys(packageJson.dependencies).map(function (module) {
@@ -83,14 +101,38 @@ gulp.task('build:node_modules', function () {
         .pipe(gulp.dest(dstSiteDir + '/lib'));
 });
 
-gulp.task('build', ['build:node_modules'].concat(copyTasks, tsBuildTasks));
+//──────────────────────────────────────────────────────────────────────────────
+// scss -> css
+//──────────────────────────────────────────────────────────────────────────────
+
+function buildCss() {
+    return gulp.src(mainScssFile)
+        .pipe($.sourcemaps.init())
+        .pipe($.sassGlob())
+        .pipe($.sass({ includePaths: '..' }).on('error', $.sass.logError))
+        .pipe($.autoprefixer({ browsers: ['last 2 versions', 'ie > 8'], cascade: false }))
+        .pipe($.sourcemaps.write())
+        .pipe(gulp.dest(dstSiteDir + '/assets/css'));
+}
+
+gulp.task('build:' + mainScssFile, () => {
+    buildCss();
+});
+
+gulp.task('watch:' + mainScssFile, () => {
+    buildCss().pipe($.livereload());
+});
+
+//──────────────────────────────────────────────────────────────────────────────
+
+gulp.task('build', ['build:node_modules', 'build:' + mainScssFile ].concat(copyTasks, tsBuildTasks));
 
 gulp.task('watch', ['build'], function() {
     $.livereload({ start: true });
     app.all(/^(?!\/dist).*$/, (req, res) => proxy.web(req, res, { target: enonic }));
     app.use(express.static(__dirname)).listen(expressPort);
-    //gulp.watch(sassFiles   , ['distCssTask']);
     //gulp.watch(assetJsFiles, ['distJsTask']);
+    gulp.watch(scssFiles, [ 'watch:' + mainScssFile ]);
     gulp.watch(watchFiles, event => { gulp.start('watch:' + event.path); });
 });
 
